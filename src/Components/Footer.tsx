facb6bab67dfa7138b906321b3f636f1f29c2057
Mohsin512 <mohsin.syed@ideofuzion.com>
2024-03-13 05:02:47 -0600

    footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -4,7 +4,7 @@ import { useTheme } from '../Theme/ThemeContext';
 const Footer = () => {
   const { theme } = useTheme();
   return (
-      <footer className={` mt-auto py-4 ${theme === 'dark' ? 'bg-light' : 'bg-dark'}` } style={{ boxShadow: 'rgba(67, 71, 85, 0.27) 0px 0px 0.25em, rgba(90, 125, 188, 0.05) 0px 0.25em 1em'}}>
+    <footer className={` mt-auto py-4 ${theme === 'dark' ? 'bg-light shadow-custom-light' : 'bg-dark shadow-custom-dark'}`}>
             <div className="container px-3">
                 <div className=" d-flex align-items-center justify-content-center text-bg-light">
                   Thank You !! For visting my portfolio.
