5908b186fea37ee9f1495e2939ab471c38518fbd
Mohsin512 <mohsin.syed@ideofuzion.com>
2024-03-12 23:43:27 -0600

    navbar

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
        <main className="flex-shrink-0 navbarStyle">
             {/* <!-- Navigation--> */}
             <nav className="navbar navbar-expand-lg navbar-light bg-white py-3">
-                <div className="container px-3">
+          <div className="container px-3">
             <Link className="navbar-brand" to="/">
               <span className="fw-bolder text-primary">PortFolio</span></Link>
             <button className="navbar-toggler" 
@@ -16,7 +16,7 @@ const Header = () => {
               <span className="navbar-toggler-icon"></span>
             </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
+                        <ul className="navbar-nav mb-2 mb-lg-0 small fw-bolder">
                             <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
                             <li className="nav-item"><Link className="nav-link" to="/resume">Resume</Link></li>
                             <li className="nav-item"><Link className="nav-link" to="/projects">Projects</Link></li>
