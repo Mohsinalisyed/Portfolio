5f2f458b2bba70708c489e115b434e5c8b53e301
MohsinAliSyed <shahmohsin873@gmail.com>
2025-03-10 02:12:21 +0500

    update changes

--- src/Home.tsx
+++ src/Home.tsx
@@ -69,7 +69,7 @@ const Home = () => {
                     My name is Mohsin Ali Syed and I help brands to grow.
                   </p>
                   <p className="text-muted">
-                  2+ year's experienced front-end developer specialized in React.js and Next.js with a robust portfolio of projects
+                  3+ year's experienced front-end developer specialized in React.js and Next.js with a robust portfolio of projects
                   evidencing expertise in UI design and development. Skilled in integrating APIs, utilizing state management tools
                   like Redux, and with a solid foundation in responsive design principles. Passionate about creating intuitive and
                   dynamic user experiences, evidenced by successful project outcomes and constant learning.                  </p>
